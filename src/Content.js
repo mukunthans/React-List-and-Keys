a31f858bc8b5b11affc2561b29e8cdb525dc4d49
Mukunthan <smukunthan134@gmail.com>
2023-01-27 14:01:43 +0530

    After lec 5

--- src/Content.js
+++ src/Content.js
@@ -4,11 +4,31 @@ const Content = () => {
     const int = Math.floor(Math.random()*3);
     return names[int];
   }
+
+  const handleClick = () => {
+    console.log("clicked");
+  }
+  const handleClick2 = (name) => {
+    console.log(`${name} clicked`);
+  }
+    const handleClick3 = (e) => {
+    console.log(e);
+    console.log(e.target.innerText);
+  }
+
+
     return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
+          {/*handleNameChange is called immediately*/ }
             Hello {handleNameChange()}
         </p>
+        {/*since inside onclick we pass reference 
+        handleClick is not called immediately*/ }
+        <button onClick={handleClick}> Click Me</button>
+        <button onClick={() => handleClick2("Mukunth")}> Click Me</button>
+        <button onClick={(event) => handleClick3(event)}> Click Me</button>
+
     </main>
     )
 }
