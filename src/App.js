524c8eb23d5aadb3dd2e2603fb0fc5e961d2ad96
Mukunthan <smukunthan134@gmail.com>
2023-01-27 20:24:59 +0530

    lec 6 final

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import Content from './Content';
 import Footer from './Footer';
 
 function App() {
-  const handleNameChange = ()=>{
-    const names =["mukunth","ganesh","mahes"];
-    const int = Math.floor(Math.random()*3);
-    return names[int];
-  }
+
   return (
     <div className="App">
      <Header/>
