7d7075fc00685aa1a4afccb20de910e6a6f9a078
Mukunthan <smukunthan134@gmail.com>
2023-01-28 22:41:30 +0530

    Lec 7 commit

--- src/Content.js
+++ src/Content.js
@@ -1,46 +1,72 @@
 import { useState } from "react";
+import {FaTrashAlt } from 'react-icons/fa';
+
 
 
 const Content = () => {
 
-    const [name,setName] = useState('Dave');
-    const [count,setCount] = useState(0);
+    const [items,setItems] = useState([
+      {
+        id:1,
+        checked:false,
+        item:"Coke"
+      },
+      {
+        id:2,
+        checked:false,
+        item:"Pepsi"
 
+      },
+            {
+        id:3,
+        checked:false,
+        item:"Fanta"
 
-      const handleNameChange = ()=>{
-    const names =["mukunth","ganesh","mahes"];
-    const int = Math.floor(Math.random()*3);
-     setName(names[int]);
-  }
+      }
+    ]);
 
-  const handleClick = () => {
-    //the count value is brought to the function and is not changed inside the function
-    setCount(count+1);
-    setCount(count+3);
+    const handleCheck = (id) => {
+        const listItems = items.map((item)=>{
+         return  item.id === id ? {...item,checked:!item.checked } : item;
+        })
+        setItems(listItems);
+        localStorage.setItem('shopingList',JSON.stringify(listItems));
+    }
+    const handleDelete = (id) => {
+      const listItems = items.filter((item)=> item.id!==id);
+      setItems(listItems);
+      localStorage.setItem('shopingList',JSON.stringify(listItems));
+    }
 
-    console.log(count);
-  }
-
-  const handleClick2 = (name) => {
-    console.log(`${name} clicked`);
-  }
-    const handleClick3 = () => {
-    console.log(count);
-
-  }
 
 
     return (
     <main>
-        <p onDoubleClick={handleClick}>
-          {/*handleNameChange is called immediately*/ }
-            Hello {name}
-        </p>
-        {/*since inside onclick we pass reference 
-        handleClick is not called immediately*/ }
-        <button onClick={handleNameChange}> Change Name</button>
-        <button onClick={handleClick}> Click Me</button>
-        <button onClick={handleClick3}> Click Me</button>
+      {items.length ? (
+      <ul>
+        {items.map((item) => {return (
+          <li className="item" key={item.id}> 
+             <input type="checkbox"
+             onChange={()=>handleCheck(item.id)}
+             checked={item.checked}>
+             </input>
+             <label
+             style={(item.checked ? {textDecoration:'line-through'} : null)}
+             onDoubleClick={()=>{handleCheck(item.id)}}
+             > {item.item}   </label>
+             <FaTrashAlt
+             onClick={()=>handleDelete(item.id)}
+             role="button"
+             tabIndex="0"/>
+             
+          </li>
+        )})}
+      </ul>
+      ): (
+        <p
+        style={{marginTop:'2rem'}}>List is empty</p>
+      )
+      }
 
     </main>
     )
